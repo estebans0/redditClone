cd31b1557d65a6854a0d1cbfee3c6c4c70029c5a
Esteban Solano <estebanjs029@gmail.com>
2024-07-02 19:23:45 -0600

    start navbar

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,28 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { authModalState } from '@/src/atoms/authModalAtom';
+
+const AuthModal: React.FC = () => {
+  const [modalState, setModalState] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
+  return (
+    <>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Here is the modal body</ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+export default AuthModal;
